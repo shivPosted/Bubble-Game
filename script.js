ac566de7ee2571abc9806271529dd5a612641072
shivPosted <shivpratap621@gmail.com>
2023-08-15 04:19:34 +0530

    done with the game 🚀

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
 "use strict";
 
 let buubleString = "";
-
+let time = 45;
+let hitOn;
+const timerUpdater = document.querySelector(".timer");
+const hitNum = document.querySelector(".hit-num");
+const scoreUpdater = document.querySelector(".score");
+const buubleContainer = document.querySelector(".bubbles");
+const retry = document.querySelector(".reset");
 const makeBubble = function () {
-  for (let i = 0; i < 160; i++) {
+  buubleString = "";
+  buubleContainer.innerHTML = "";
+  for (let i = 0; i < 159; i++) {
     const rn = Math.floor(Math.random() * 9 + 1);
     buubleString += `<span class="bubble">${rn}</span>`;
   }
+  buubleContainer.insertAdjacentHTML("afterbegin", buubleString);
+  hitNumUpdater();
 };
 
+const hitNumUpdater = function () {
+  hitOn = Math.floor(Math.random() * 9 + 1);
+  hitNum.textContent = hitOn;
+};
 makeBubble();
-document.querySelector(".bubbles").innerHTML = buubleString;
+function timerCount() {
+  setTimeout(() => {
+    time--;
+    timerUpdater.textContent = time;
+  }, 1000);
+  const timeInt = setInterval(() => {
+    if (timerUpdater.textContent === "0") {
+      buubleContainer.classList.add("hidden");
+      document.querySelector(".time-up").classList.remove("hidden");
+    }
+    if (time < 0) clearInterval(timeInt);
+    else {
+      timerUpdater.textContent = time;
+    }
+    time--;
+  }, 1000);
+}
+timerCount();
+buubleContainer.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("bubble")) return;
+
+  if (
+    Number(e.target.textContent) === hitOn &&
+    timerUpdater.textContent !== "0"
+  ) {
+    makeBubble();
+    scoreUpdater.textContent = Number(scoreUpdater.textContent) + 10;
+  }
+});
+
+retry.addEventListener("click", function () {
+  buubleContainer.classList.remove("hidden");
+  document.querySelector(".time-up").classList.add("hidden");
+  makeBubble();
+  scoreUpdater.textContent = 0;
+  timerUpdater.textContent = 45;
+  time = 45;
+  timerCount();
+});
